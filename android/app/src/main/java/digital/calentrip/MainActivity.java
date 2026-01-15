fd976b668d1de782b1897f0ad04d67ca4df22aa8
edbckclaudio <edbckclaudio@example.com>
2026-01-15 14:18:40 -0300

    Android: WebView renderização por software e windowBackground sólido

--- android/app/src/main/java/digital/calentrip/MainActivity.java
+++ android/app/src/main/java/digital/calentrip/MainActivity.java
@@ -1,3 +1,12 @@
 package digital.calentrip.android;
 import com.getcapacitor.BridgeActivity;
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(android.os.Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    android.webkit.WebView webView = getBridge().getWebView();
+    if (webView != null) {
+      webView.setLayerType(android.view.View.LAYER_TYPE_SOFTWARE, null);
+    }
+  }
+}
