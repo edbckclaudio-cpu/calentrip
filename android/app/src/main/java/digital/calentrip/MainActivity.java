118d609fda589261fa54790bbd274c0f0891c3c3
edbckclaudio <edbckclaudio@example.com>
2026-01-18 17:31:25 -0300

    chore(android): versionCode 162, versionName 1.3.14; integrar GoogleAuth no calentrip

--- android/app/src/main/java/digital/calentrip/MainActivity.java
+++ android/app/src/main/java/digital/calentrip/MainActivity.java
@@ -1,3 +1,14 @@
 package digital.calentrip.android;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-public class MainActivity extends BridgeActivity {}
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    try {
+      registerPlugin(GoogleAuth.class);
+    } catch (Exception ignored) {}
+  }
+}
